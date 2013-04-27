052b719a414fe557dd357fe7efbf75f32821e402
Elbert Alias <elbert@alias.io>
2013-04-27 18:42:51 +1000

    Use proper prototypes

--- js/tetr.js
+++ js/tetr.js
@@ -1,481 +1,472 @@
 (function($) {
-	var app = {
-		blockSize: { main: 30, next: 15 },
-		el: {},
-		grid: null,
-		shapes: [ 'ddd', 'drr', 'ddr', 'drd', 'rdr', 'dru', 'rdur' ],
-		delay: 1000,
-		lastId: 0,
-		tetrimino: null,
-		queue: [],
+	$(function() { new Tetris; });
 
-		/**
-		 * Initialise
-		 */
-		init: function() {
-			app.el = {
-				main: $('#main'),
-				queue: $('#queue')
+	var Tetris = function() {
+		var self = this;
+
+		this.shapes    = [ 'ddd', 'drr', 'ddr', 'drd', 'rdr', 'dru', 'rdur' ];
+		this.blockSize = { main: 30, queue: 15 };
+		this.delay     = 1000;
+		this.lastId    = 0;
+		this.tetrimino = null;
+		this.queue     = [];
+
+		this.el = {
+			main:  $('#main'),
+			queue: $('#queue')
 			};
 
-			app.grid = new app.Grid(10, 20);
-
-			_(_.range(6)).each(function() {
-				app.queue.push(new app.Tetrimino);
-			});
-
-			app.next();
-
-			// Key bindings
-			KeyboardJS.on('w, up',    function() { app.tetrimino.rotate().render(); });
-			KeyboardJS.on('a, left',  function() { app.tetrimino  .left().render(); });
-			KeyboardJS.on('s, down',  function() { app.tetrimino  .down().render(); });
-			KeyboardJS.on('d, right', function() { app.tetrimino .right().render(); });
-
-			KeyboardJS.on('space', function() { app.tetrimino  .drop().render(); });
-
-			app.interval = setInterval(app.progress, app.delay);
-		},
-
-		/**
-		 *
-		 */
-		Tetrimino: function() {
-			var
-				move,
-				self   = this,
-				blocks = [],
-				x      = 0,
-				y      = 0
-				;
-
-			self.id = ++ app.lastId;
-
-			// Generate a random tetrimino
-			self.shape = app.shapes[_.random(app.shapes.length - 1)];
-
-			blocks.push(new app.Block(self, x, y));
-
-			_.map(self.shape.split(''), function(direction) {
-				switch ( direction ) {
-					case 'r': x ++; break;
-					case 'd': y ++; break;
-					case 'u': y --; break;
-				}
-
-				blocks.push(new app.Block(self, x, y));
-			});
-
-			/**
-			 *
-			 */
-			move = function(x, y) {
-				_.each(blocks, function(block) {
-					block.pos.x += x;
-					block.pos.y += y;
-
-					app.grid.place(block);
-				});
-
-				/*
-				if ( self.collision() && y ) {
-					return app.land();
-				}
-				*/
-
-				return self;
+		this.field = {
+			main:  new Field(this, 'main',  10, 20),
+			queue: new Field(this, 'queue', 99, 4)
 			};
 
-			/**
-			 *
-			 */
-			self.left = function() {
-				console.log('left');
+		$.each(new Array(6), function() {
+			self.queueAdd();
+		});
 
-				return move(-1, 0);
-			};
+		this.next();
 
-			/**
-			 *
-			 */
-			self.right = function() {
-				console.log('right');
+		// Key bindings
+		Mousetrap.bind('space',        function() { self.tetrimino.drop()     .render(); });
+		Mousetrap.bind(['w', 'up'   ], function() { self.tetrimino.rotate()   .render(); });
+		Mousetrap.bind(['a', 'left' ], function() { self.tetrimino.move(-1, 0).render(); });
+		Mousetrap.bind(['s', 'down' ], function() { self.tetrimino.move( 0, 1).render(); });
+		Mousetrap.bind(['d', 'right'], function() { self.tetrimino.move( 1, 0).render(); });
 
-				return move(1, 0);
-			};
+		this.interval = setInterval(function() { self.progress(self); }, this.delay);
 
-			/**
-			 *
-			 */
-			self.down = function() {
-				console.log('down');
+		return this;
+	};
 
-				return move(0, 1);
-			};
+	/**
+	 *
+	 */
+	Tetris.prototype.progress = function(self) {
+		//self.tetrimino.move(0, 1).render();
+	};
 
-			/**
-			 *
-			 */
-			self.drop = function() {
-				console.log('drop');
+	/**
+	 *
+	 */
+	Tetris.prototype.land = function(tetrimino) {
+		var
+			self       = this,
+			filledCols = {},
+			filledRows = []
+			;
 
-				var id = self.id;
+		tetrimino.render();
 
-				while ( self.down().id === id );
+		// Check main field for completed lines
+		this.field.main.each(function(x, y, block) {
+			if ( filledCols[y] === undefined ) {
+				filledCols[y] = 0;
+			}
 
-				return self.render();
-			};
+			filledCols[y] += block ? 1 : 0;
 
-			/**
-			 *
-			 */
-			self.rotate = function() {
-				console.log('rotate');
+			if ( filledCols[y] === self.field.main.cols ) {
+				filledRows.push(y);
+			}
+		});
 
-				var
-					oldGrid = _.clone(self.grid.grid),
-					grid    = new app.Grid(4, 4)
-					;
+		// Remove completed lines
+		if ( filledRows ) {
+			$.forEach(filledRows, function(row) {
+				self.field.main.each(function(x, y, block) {
+					if ( y === row ) {
+						block.el.fadeOut(x * 50, function() {
+							self.field.main.clear(x, y);
 
-				self.grid.map(function(col, row, block) {
-					if ( block ) {
-						grid.fill(3 - row, col, block);
+							// Drop above rows
+							self.field.main.each(function(x2, y2, block) {
+								if ( block && y2 < y ) {
+									self.field.main.move(block, x2, y2 + 1);
+
+									block.render();
+								}
+							});
+						});
 					}
 				});
-
-				self.grid.grid = grid.grid;
-
-				//self.grid.trim();
-
-				return self;
-			};
-
-			/**
-			 *
-			 */
-			self.collision = function() {
-				_.each(blocks, function(block) {
-					block.collision();
-				});
-
-				return self;
-			};
-
-			/**
-			 *
-			 */
-			self.render = function() {
-				blocks.map(function(block) {
-					block.render();
-				});
-
-				return self;
-			};
-		},
-
-		Block: function(tetrimino, x, y) {
-			var self = this;
-
-			self.tetrimino = tetrimino;
-
-			self.pos = { x: x, y: y };
-
-			self.oldPos = _.clone(self.pos);
-
-			self.el = $('<div>').addClass('block').addClass(tetrimino.shape);
-
-			app.el.main.append(self.el);
-
-			/**
-			 *
-			 */
-			self.collision = function() {
-				self.pos = _.clone(self.oldPos);
-			}
-
-			/**
-			 *
-			 */
-			self.render = function() {
-				self.oldPos = _.clone(self.pos);
-
-				self.el
-					.stop()
-					.appendTo(app.el.main)
-					.css({
-						left: self.pos.x * app.blockSize.main,
-						top:  self.pos.y * app.blockSize.main
-					});
-			}
-
-			return self;
-		},
-
-		/**
-		 *
-		 */
-		progress: function() {
-			app.tetrimino.down().render();
-		},
-
-		/**
-		 *
-		 */
-		land: function() {
-			var
-				filledCols = {},
-				filledRows = []
-				;
-
-			app.tetrimino.render();
-
-			// Check main grid for completed lines
-			app.grid.map(function(col, row, block) {
-				if ( _.isUndefined(filledCols[row]) ) {
-					filledCols[row] = 0;
-				}
-
-				filledCols[row] += block ? 1 : 0;
-
-				if ( filledCols[row] === app.grid.cols ) {
-					filledRows.push(row);
-				}
 			});
-
-			// Remove completed lines
-			if ( filledRows ) {
-				_.map(filledRows, function(row) {
-					app.grid.map(function(x, y, block) {
-						if ( y === row ) {
-							block.el.fadeOut(x * 50, function() {
-								app.grid.clear(x, y);
-
-								app.grid.map(function(x2, y2, block) {
-									if ( !_.isNull(block) && y2 < y ) {
-										app.grid.clear(x2, y2, block);
-										app.grid.fill(x2, y2 + 1, block);
-
-										setTimeout(function() {
-											block.el
-												.stop()
-												.animate({
-													top: ( y2 + 1 ) * app.blockSize.main
-												}, 50);
-										}, x2 * 50);
-									}
-								});
-							});
-						}
-					});
-				});
-			}
-
-			// Check for game over
-			app.grid.map(function(col, row, block) {
-				if ( block && row === 0 ) {
-					console.log(row);
-					app.gameOver();
-				}
-			});
-
-			return app.next();
-		},
-
-		/**
-		 *
-		 */
-		next: function() {
-			console.log('next');
-
-			var posX = 0;
-
-			if ( app.tetrimino ) {
-				app.tetrimino.render();
-			}
-
-			app.tetrimino = app.queue.shift();
-
-			app.queue.push(new app.Tetrimino);
-
-			_.each(app.queue, function(tetrimino, i) {
-				_.each(tetrimino.blocks, function(block) {
-					if ( block.el.parent()[0] !== app.el.queue[0] ) {
-						app.el.queue.append(block.el);
-
-						block.el
-							.css({
-								left: ( col + posX ) * app.blockSize.next,
-								top:  row            * app.blockSize.next
-							});
-					}
-
-					block.el
-						.stop()
-						.animate({
-							left: ( col + posX ) * app.blockSize.next
-						}, 'fast')
-						;
-				});
-			});
-
-
-			return app.tetrimino.render();
-		},
-
-		/**
-		 *
-		 */
-		gameOver: function() {
-			console.log('gameOver');
-
-			clearInterval(app.interval);
-		},
-
-		/**
-		 * Create a null-filled grid
-		 */
-		Grid: function(cols, rows) {
-			var
-				//trim,
-				self = this
-				;
-
-			self.cols = cols;
-			self.rows = rows;
-
-			self.grid = _(_.range(cols)).map(function() {
-				return _(_.range(rows)).map(function() {
-					return null;
-				});
-			});
-
-			/**
-			 * Check if a cell is empty
-			 *
-			 * @param integer x
-			 * @param integer y
-			 * @return boolean
-			 */
-			self.isFilled = function(x, y) {
-				return ( x < 0 || x > self.grid.cols || y > 0 ) && ( _.isUndefined(self.grid[x]) || _.isUndefined(self.grid[x][y]) || self.grid[x][y] );
-			};
-
-			/**
-			 *
-			 */
-			self.place = function(block) {
-				if ( !self.isFilled(block.pos.x, block.pos.y) ) {
-					self.grid[block.pos.x][block.pos.y] = block;
-				} else {
-					block.tetrimino.collision();
-				}
-
-				return self;
-			};
-
-			/**
-			 *
-			 */
-			/*
-			self.fill = function(x, y, block) {
-				if ( !self.isFilled(x, y) ) {
-					self.grid[x][y] = block;
-
-					block.pos = { x: x, y: y };
-				} else {
-					block.el.remove();
-				}
-
-				return self;
-			};
-			*/
-
-			/**
-			 *
-			 */
-			self.clear = function(x, y, block) {
-				if ( self.isFilled(x, y) ) {
-					self.grid[x][y].el.remove();
-
-					self.grid[x][y] = null;
-				}
-
-				return self;
-			};
-
-			/**
-			 *
-			 */
-			/*
-			self.move = function(x, y, block) {
-				if ( self.isFilled(x, y) ) {
-					self.grid[x][y].el.remove();
-
-					self.grid[x][y] = null;
-				}
-
-				return self;
-			};
-			*/
-
-			/**
-			 *
-			 */
-			self.map = function(callback) {
-				_.each(self.grid, function(col, x) {
-					_.each(col, function(block, y) {
-						callback(x, y, block);
-					});
-				});
-			};
-
-			/**
-			 *
-			 */
-			/*
-			self.trim = function() {
-				self.trimmed = new app.Grid(4, 4);
-
-				self.trimmed.grid = _.clone(self.grid);
-
-				trim();
-
-				return self;
-			};
-			*/
-
-			/**
-			 *
-			 */
-			/*
-			trim = function() {
-				var
-					g      = self.trimmed.grid,
-					top    = true,
-					right  = true,
-					bottom = true,
-					left   = true
-					;
-
-				_.each(g[0],            function(col) { if ( col                 ) { right  = false; } });
-				_.each(g[g.length - 1], function(col) { if ( col                 ) { left   = false; } });
-				_.each(g,               function(row) { if ( row[0]              ) { top    = false; } });
-				_.each(g,               function(row) { if ( row[row.length - 1] ) { bottom = false; } });
-
-				if ( right  ) { g.shift();                                 self.trimmed.cols --; }
-				if ( left   ) { g.pop();                                   self.trimmed.cols --; }
-				if ( top    ) { _.each(g, function(row) { row.shift(); }); self.trimmed.rows --; }
-				if ( bottom ) { _.each(g, function(row) { row.pop();   }); self.trimmed.rows --; }
-
-				if ( top || right || bottom || left ) {
-					trim();
-				}
-			};
-			*/
-
-			return self;
 		}
-	}
 
-	$(app.init);
+		// Check for tetris over
+		this.field.main.each(function(x, y, block) {
+			if ( !y && block ) {
+				self.gameOver();
+			}
+		});
+
+		return this.next();
+	};
+
+	/**
+	 *
+	 */
+	Tetris.prototype.next = function() {
+		var pos;
+
+		console.log('next');
+
+		this.tetrimino = this.queue.shift();
+
+		pos = this.tetrimino.pos();
+
+		$.each(this.tetrimino.blocks, function() {
+			this.tetrimino.field.grid[this.pos.x][this.pos.y] = null;
+
+			this.pos.x -= pos.x;
+			this.pos.y -= pos.y;
+		});
+
+		this.tetrimino.field = this.field.main;
+
+		$.each(this.tetrimino.blocks, function() {
+			this.tetrimino.field.place(this);
+		});
+
+		this.tetrimino.render();
+
+		this.queueAdd();
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Tetris.prototype.queueAdd = function() {
+		var
+			tetrimino = new Tetrimino(this.field.queue),
+			offset    = 0
+			;
+
+		$.each(this.queue, function() {
+			offset += this.size().x + 2;
+		});
+
+		this.queue.push(tetrimino);
+
+		tetrimino.move(offset, 0);
+
+		return tetrimino.render();
+	};
+
+	/**
+	 *
+	 */
+	Tetris.prototype.gameOver = function() {
+		console.log('gameOver');
+
+		clearInterval(this.interval);
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	var Tetrimino = function(field) {
+		var
+			self = this,
+			x    = 0,
+			y    = 0
+			;
+
+		this.field  = field;
+		this.tetris = field.tetris;
+
+		this.id = ++ this.tetris.lastId;
+
+		this.blocks = [];
+
+		// Generate a random tetrimino
+		this.shape = this.tetris.shapes[Math.ceil(Math.random() * this.tetris.shapes.length - 1)];
+
+		this.blocks.push(new Block(this, x, y));
+
+		$.each(this.shape.split(''), function() {
+			switch ( this.toString() ) {
+				case 'r': x ++; break;
+				case 'd': y ++; break;
+				case 'u': y --; break;
+			}
+
+			self.blocks.push(new Block(self, x, y));
+		});
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Tetrimino.prototype.move = function(x, y) {
+		var self = this;
+
+		$.each(this.blocks, function() {
+			if ( self.field.available(this.pos.x + x, this.pos.y + y, this) ) {
+				self.field.grid[this.pos.x][this.pos.y] = null;
+			}
+
+			this.pos.x += x;
+			this.pos.y += y;
+		});
+
+		$.each(this.blocks, function() {
+			return self.field.place(this);
+		});
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Tetrimino.prototype.drop = function() {
+		console.log('drop');
+
+		//while ( self.id === tetris.tetrimino.id ) {
+			this.move(0, 1);
+		//}
+
+		return this.render();
+	};
+
+	/**
+	 *
+	 */
+	Tetrimino.prototype.rotate = function() {
+		console.log('rotate');
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Tetrimino.prototype.collision = function() {
+		var
+			self = this,
+			land = false
+			;
+
+		if ( this.field.id !== 'queue' ) {
+			console.log('collision');
+		}
+
+		$.each(this.blocks, function() {
+			this.collision();
+
+			land = land || this.pos.y === self.tetris.field.rows - 1 || this.pos.y > this.oldPos.y;
+		});
+
+		$.each(this.blocks, function() {
+			//self.field.place(this);
+		});
+
+		if ( land ) {
+			this.tetris.land(this);
+		}
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Tetrimino.prototype.pos = function() {
+		var pos = { x: Infinity, y: Infinity };
+
+		$.each(this.blocks, function() {
+			pos = {
+				x: Math.min(pos.x, this.pos.x),
+				y: Math.min(pos.y, this.pos.y)
+				};
+		});
+
+		return pos;
+	};
+
+	/**
+	 *
+	 */
+	Tetrimino.prototype.size = function() {
+		var max = { x: 0, y: 0 };
+
+		$.each(this.blocks, function() {
+			max = {
+				x: Math.max(max.x, this.pos.x),
+				y: Math.max(max.y, this.pos.y)
+				};
+		});
+
+		return { x: max.x - this.pos().x, y: max.y - this.pos().y };
+	};
+
+	/**
+	 *
+	 */
+	Tetrimino.prototype.render = function() {
+		this.blocks.map(function(block) {
+			block.render();
+		});
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	var Block = function(tetrimino, x, y) {
+		this.tetrimino = tetrimino;
+		this.tetris    = tetrimino.tetris;
+
+		this.pos    = { x: x, y: y };
+		this.oldPos = { x: x, y: y };
+
+		this.el = $('<div>').addClass('block').addClass(tetrimino.shape).attr('data-tetrimino-id', tetrimino.id);
+
+		tetrimino.field.place(this);
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Block.prototype.collision = function() {
+		this.pos = $.extend({}, this.oldPos);
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Block.prototype.render = function() {
+		this.oldPos = $.extend({}, this.pos);
+
+		this.el
+			.stop()
+			.appendTo(this.tetris.el[this.tetrimino.field.id])
+			.css({
+				left: this.pos.x * this.tetris.blockSize[this.tetrimino.field.id],
+				top:  this.pos.y * this.tetris.blockSize[this.tetrimino.field.id]
+			});
+
+		return this;
+	};
+
+	/**
+	 * Create an empty field
+	 */
+	Field = function(tetris, id, cols, rows) {
+		var self = this;
+
+		this.tetris = tetris;
+		this.cols   = cols;
+		this.rows   = rows;
+		this.id     = id;
+		this.grid   = [];
+
+		$.each(Array(cols), function(x) {
+			self.grid[x] = [];
+
+			$.each(Array(rows), function(y) {
+				self.grid[x][y] = null;
+			});
+		});
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Field.prototype.valid = function(x, y) {
+		return typeof this.grid[x] !== 'undefined' && typeof this.grid[x][y] !== 'undefined';
+	};
+
+	/**
+	 * Check if a cell is empty
+	 *
+	 * @param integer x
+	 * @param integer y
+	 * @param object block
+	 * @return boolean
+	 */
+	Field.prototype.available = function(x, y, block) {
+		if ( !this.valid(x, y) ) {
+			return false;
+		}
+
+		if ( !this.grid[x][y] ) {
+			return true;
+		}
+
+		if ( block && block.tetrimino && this.grid[x][y].tetrimino ) {
+			return this.grid[x][y].tetrimino === block.tetrimino;
+		}
+
+		return false;
+	};
+
+	/**
+	 *
+	 */
+	Field.prototype.place = function(block) {
+		if ( this.available(block.pos.x, block.pos.y, block) ) {
+			this.grid[block.pos.x][block.pos.y] = block;
+		} else {
+			block.tetrimino.collision();
+		}
+
+		//
+		if ( this.id === 'main' ) {
+			var grid = '';
+
+			$.each(this.grid, function(x) {
+				$.each(this, function(y) {
+					grid += this instanceof Block ? 'X' : '.';
+				});
+
+				grid += "\n";
+			});
+
+			$('#debug').text(grid);
+		}
+		//
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Field.prototype.clear = function(x, y, block) {
+		if ( this.valid(x, y) && !this.available(x, y) ) {
+			this.grid[x][y].el.remove();
+
+			this.grid[x][y] = null;
+		}
+
+		return this;
+	};
+
+	/**
+	 *
+	 */
+	Field.prototype.each = function(callback) {
+		$.each(this.grid, function(x) {
+			$.each(this, function(y) {
+				callback(x, y, this);
+			});
+		});
+
+		return this;
+	};
 })(jQuery);
